4e7995d977359bd75b9400d6ddb32092e694e8d9
SoundtrkSamurai <furtimat3@yahoo.com>
2023-08-20 00:17:38 -0700

    main: finish lesson one

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
+import Alert from './components/Alert';
+import Button from './components/Button';
+import { useState } from 'react';
 
 function App() {
-  let cities = ["New York", "San Francisco", "Dallas", "Paris", "Tokyo"];
 
-  const handleSelectItem = (item: string) => console.log(item);
+const [alertVisible, setAlertVisible] = useState(false);
 
-  return <div><ListGroup items={cities} heading="Cities" onSelectItem={handleSelectItem}/></div>
+//   let cities = ["New York", "San Francisco", "Dallas", "Paris", "Tokyo"];
+//   const handleSelectItem = (item: string) => console.log(item);
+//   return <div><ListGroup items={cities} heading="Cities" onSelectItem={handleSelectItem}/></div>
+  
+  return (
+    <>
+      {alertVisible && <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>}
+      <Button color="danger" onClick={() => setAlertVisible(true)}>My Button</Button>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+interface Props {
+    children: string;
+    onClick: () => void;
+    color?: 'primary' | 'danger' | 'secondary';
+}
+
+const Button = ({ children, onClick, color='primary'}: Props) => {
+  return (
+    <button className={'btn btn-' + color} onClick={onClick}>{children}</button>
+  )
+}
+
+export default Button
