6d37a05a224ed5bc2e0b7500e4d075db99b9e51f
SoundtrkSamurai <furtimat3@yahoo.com>
2023-08-21 13:55:13 -0700

    styling: learned styling components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
 // import ListGroup from "./components/ListGroup";
 import Alert from './components/Alert';
 import Button from './components/Button';
+import ListGroup from './components/ListGroup';
+import { BsFillCalendarFill } from 'react-icons/bs';
 import { useState } from 'react';
+import Like from './components/Like';
+
+import './App.scss';
 
 function App() {
 
-const [alertVisible, setAlertVisible] = useState(false);
+// const [alertVisible, setAlertVisible] = useState(false);
 
-//   let cities = ["New York", "San Francisco", "Dallas", "Paris", "Tokyo"];
-//   const handleSelectItem = (item: string) => console.log(item);
-//   return <div><ListGroup items={cities} heading="Cities" onSelectItem={handleSelectItem}/></div>
+  // let cities = ["New York", "San Francisco", "Dallas", "Paris", "Tokyo"];
+  // const handleSelectItem = (item: string) => console.log(item);
   
+  // return <div><ListGroup items={cities} heading="Cities" onSelectItem={handleSelectItem}/></div>
+  
+  // return (
+  //   <>
+  //     {alertVisible && <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>}
+  //     <Button color="danger" onClick={() => setAlertVisible(true)}>My Button</Button>
+  //   </>
+  // )
+
+  // return (
+  //   <>
+  //     <BsFillCalendarFill color="red" size="40" />
+  //   </>
+  // );
+
   return (
-    <>
-      {alertVisible && <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>}
-      <Button color="danger" onClick={() => setAlertVisible(true)}>My Button</Button>
-    </>
+    <><Like onClick={() => console.log('Clicked')}/></>
   )
 }
 
