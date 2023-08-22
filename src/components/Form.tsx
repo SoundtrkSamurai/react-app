81d5c7859830ad74b56d0fc1205a0dae31d4fc9f
SoundtrkSamurai <furtimat3@yahoo.com>
2023-08-22 16:52:32 -0700

    forms: tie form values to useState

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,30 +1,50 @@
-import React, { FormEvent, useRef } from 'react'
+import React, { FormEvent, useState } from "react";
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person =  {name: '', age: 0};
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
 
   const handleSumbit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current?.value) person.name = nameRef.current.value;
-    if (ageRef.current?.value) person.age = parseInt(ageRef.current.value)
-
     console.log(person);
-  }
+  };
   return (
     <form onSubmit={handleSumbit}>
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">Name</label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          value={person.name}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
-        <label htmlFor="age" className="form-label">Age</label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          value={person.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
-      <button type="submit" className="btn btn-primary">Submit</button>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
