77e94981bbadbfaad2828403a8a3c11d6810c1dd
SoundtrkSamurai <furtimat3@yahoo.com>
2023-08-22 19:03:35 -0700

    forms: forms demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 // import ListGroup from "./components/ListGroup";
 // import Alert from './components/Alert';
@@ -7,21 +7,40 @@ import { useState } from 'react';
 // import Like from './components/Like';
 // import NavBar from './components/NavBar';
 // import Carts from './components/Carts';
-import Form from './components/Form';
+// import Form from './components/Form';
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import Expensefilter from "./expense-tracker/components/Expensefilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 
 // import { BsFillCalendarFill } from 'react-icons/bs';
 
-import './App.scss';
+import "./App.scss";
+
+export const categories = ["Groceries", "Utilities", "Entertainment"];
 
 function App() {
-
   // const [alertVisible, setAlertVisible] = useState(false);
+
   // const [products, setProducts] = useState(['Product 1', 'Product 2']);
+
   // let cities = ["New York", "San Francisco", "Dallas", "Paris", "Tokyo"];
   // const handleSelectItem = (item: string) => console.log(item);
-  
+
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "baa", amount: 11, category: "Utilities" },
+    { id: 3, description: "caa", amount: 12, category: "Utilities" },
+    { id: 4, description: "daa", amount: 13, category: "Utilities" },
+  ]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const visibleExpenses = selectedCategory
+    ? expenses.filter(
+        (e) => e.category.toLowerCase() === selectedCategory.toLowerCase()
+      )
+    : expenses;
+
   // return <div><ListGroup items={cities} heading="Cities" onSelectItem={handleSelectItem}/></div>
-  
+
   // return (
   //   <>
   //     {alertVisible && <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>}
@@ -46,11 +65,30 @@ function App() {
   //   </div>
   // )
 
+  // return (
+  //   <div>
+  //     <Form />
+  //   </div>
+  // )
+
   return (
     <div>
-      <Form />
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
+      <div className="mb-3">
+        <Expensefilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      />
     </div>
-  )
+  );
 }
 
 export default App;
